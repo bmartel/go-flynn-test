9862f1a5f3b88bcfa356ffdd42751c3f467c5555
Brandon Martel <brandonmartel@gmail.com>
2016-10-10 19:45:05 -0500

    intial commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	port := os.Getenv("PORT")
+	if port != "" {
+		port = "8080"
+	}
+	r.Run(":" + port)
+}
